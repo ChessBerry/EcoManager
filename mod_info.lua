673f3a8c0c4b7fd7d90b3c38b7cd9d1ad68e9796
CheeseBerry <cheeseberry@protonmail.com>
2023-08-18 02:21:24 +0200

    Change name to EcoManager-CBT

--- mod_info.lua
+++ mod_info.lua
@@ -1,9 +1,9 @@
-name = "EcoManager"
-uid = "ecbf6277-24e3-597a-b968-EcoManager-v12"
-version = 12
+name = "EcoManager-CBT"
+uid = "ecbf6277-24e3-597a-b968-EcoManager-CBT-v1"
+version = 1
 copyright = "Crotalus"
-description = "EcoManager, helps with micromanaging the economy. v12: Fix the black box of death bug"
-author = "Crotalus"
+description = "EcoManager, helps with micromanaging the economy. CheeseBerryTweaks v1"
+author = "Crotalus, CheeseBerry"
 url = ""
 selectable = true
 enabled = true
@@ -14,18 +14,19 @@ requires = {
 }
 requiresNames = {}
 conflicts = {
-"ecbf6277-24e3-437a-b968-EcoManager-v11",
-"ecbf6277-24e3-437a-b968-EcoManager-v10",
-"ecbf6277-24e3-437a-b968-EcoManager-v9",
-"ecbf6277-24e3-437a-b968-EcoManager-v8",
-"ecbf6277-24e3-437a-b968-EcoManager-v7",
-"ecbf6277-24e3-437a-b968-EcoManager-v6",
-"ecbf6277-24e3-437a-b968-EcoManager-v5",
-"ecbf6277-24e3-437a-b968-EcoManager-v4",
-"ecbf6277-24e3-437a-b968-75e7e7c67db9",
-"21188cdf-ec69-43bd-b756-6140a84c0197",
-"5c1ac506-03d3-4734-b6c1-0746592849c3",
-"5f56b6fd-de36-44f1-ba2b-e7d012025f2a"
+    "ecbf6277-24e3-597a-b968-EcoManager-v12",
+    "ecbf6277-24e3-437a-b968-EcoManager-v11",
+    "ecbf6277-24e3-437a-b968-EcoManager-v10",
+    "ecbf6277-24e3-437a-b968-EcoManager-v9",
+    "ecbf6277-24e3-437a-b968-EcoManager-v8",
+    "ecbf6277-24e3-437a-b968-EcoManager-v7",
+    "ecbf6277-24e3-437a-b968-EcoManager-v6",
+    "ecbf6277-24e3-437a-b968-EcoManager-v5",
+    "ecbf6277-24e3-437a-b968-EcoManager-v4",
+    "ecbf6277-24e3-437a-b968-75e7e7c67db9",
+    "21188cdf-ec69-43bd-b756-6140a84c0197",
+    "5c1ac506-03d3-4734-b6c1-0746592849c3",
+    "5f56b6fd-de36-44f1-ba2b-e7d012025f2a"
 }
 before = {"zcbf6277-24e3-437a-b968-Common-v1"}
 after = {}
