67cfd184c72a19bc8dd61ea4df1496be16029cc8
CheeseBerry <cheeseberry@protonmail.com>
2024-02-18 18:53:55 +0100

    Update UID to follow FAF guidelines

--- mod_info.lua
+++ mod_info.lua
@@ -1,5 +1,5 @@
 name = "EcoManagerCBT"
-uid = "ecbf6277-24e3-597a-EcoManagerCBT-v2"
+uid = "8dee7aa0-ce86-11ee-a2f8-0800200c9a66"
 version = 2
 copyright = "Crotalus"
 description = "EcoManager, helps with micromanaging the economy. CheeseBerryTweaks v1"
@@ -10,7 +10,7 @@ enabled = true
 exclusive = false
 ui_only = true
 requires = {
-    "zcbf6277-24e3-437a-b968-Common-v1"
+    "zcbf6277-24e3-437a-b968-Common-v1"  -- Common Mod Tools
 }
 requiresNames = {}
 conflicts = {
