8115a904af19eb0a4883123ea659a324d1e0147d
CheeseBerry <cheeseberry@protonmail.com>
2024-04-18 02:35:19 +0200

    Bump version and UID

--- mod_info.lua
+++ mod_info.lua
@@ -1,6 +1,6 @@
 name = "EcoManagerCBT"
-uid = "c37fc150-ce96-11ee-EcoManagerCBT-v5"
-version = 5
+uid = "c37fc150-ce96-11ee-EcoManagerCBT-v6"
+version = 6
 copyright = "Crotalus"
 description = "EcoManager, helps with micromanaging the economy. CheeseBerryTweaks"
 author = "Crotalus, CheeseBerry"
@@ -14,6 +14,7 @@ requires = {
 }
 requiresNames = {}
 conflicts = {
+    "c37fc150-ce96-11ee-EcoManagerCBT-v5",
     "c37fc150-ce96-11ee-a2f8-0800200c9a66",
     "ecbf6277-24e3-597a-EcoManagerCBT-v1",
     "ecbf6277-EcoManagerCBT-v1",
